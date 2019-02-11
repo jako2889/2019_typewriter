ea05e1c694cc1286d45b2e96383d56244b639ddd
Jakob <jako2889@stud.kea.dk>
2019-02-11 18:23:30 +0100

    har lavet start og stop knap - fuck sound

--- typewriter.js
+++ typewriter.js
@@ -12,7 +12,13 @@ let Text = document.querySelector("#typewriter").textContent;
 
 let counter = 0;
 
-let sound = new Audio("typekey1.mp3");
+const space = document.querySelector("#typewriter").textContent.substring(4, 5);
+
+const sound = new Audio("typekey1.mp3");
+const soundspace = new Audio("typespace.mp3");
+
+const button = document.querySelector("#button");
+const reset = document.querySelector("#reset");
 
 console.log(Text + counter);
 
@@ -23,35 +29,49 @@ function init() {
 
   // reset counter
 
+  counter = 0;
+
   // start loop
 
   document.querySelector("#typewriter").textContent = "";
 
   console.log(Text);
-  loop();
+  button.addEventListener("click", loop);
+  reset.addEventListener("click", reload);
 }
 
+// reset page
+function reload() {
+  location.reload();
+}
+
+//loop find text from 0 to counter ----- function
+
 function loop() {
+  // display textpart in html
   const part = Text.substring(0, counter + 1);
+
+  // counter++
   counter++;
   console.log(counter);
 
   document.querySelector("#typewriter").textContent = part;
-  sound.play();
 
+  // if text.length > counter = loop
+  if (part == space) {
+    soundspace.play();
+    console.log("soundspace");
+  } else {
+    sound.play();
+    console.log("sound");
+  }
+
+  // set timeout / delay / 400ms
   if (Text.length > counter) {
     setTimeout(loop, 400);
+  } else {
+    setTimeout(init, 1000);
   }
 }
 
 //----------------------------------------------
-
-//loop find text from 0 to counter ----- function
-
-// display textpart in html
-
-// counter++
-
-// if text.length > counter = loop
-
-// set timeout / delay / 400ms
